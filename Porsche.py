a902f6ed9d96531cf88d511fc418a620c32e9bb6
leral3 <leral3@naver.com>
2022-03-28 00:17:56 +0900

    가격을 입력받는 set_price 메서드 추가

--- Porsche.py
+++ Porsche.py
@@ -9,8 +9,13 @@ class Porsche:
     def set_name(self, model):
         self.model = model
 
+    def set_price(self, price):
+        self.price = price
+
 포르쉐 = Porsche(None, None)
 
 포르쉐.set_name("카이엔")
+포르쉐.set_price("2억")
 
 print(포르쉐.model)
+print(포르쉐.price)
