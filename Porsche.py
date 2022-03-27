c6f6ff2a53e17efd8f9a6a7dd8c0a92085192fae
leral3 <leral3@naver.com>
2022-03-28 00:11:03 +0900

    Porsche 클래스의 생성자 정의

--- Porsche.py
+++ Porsche.py
@@ -1,2 +1,10 @@
 class Porsche:
-    pass
+    def __init__(self, model, price):
+        self.model = model
+        self.price = price
+
+
+포르쉐 = Porsche("카이엔", "2억")
+
+print(포르쉐.model)
+print(포르쉐.price)
