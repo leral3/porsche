df541a8af8fd3590e8b91e10abe6a78dd999be85
leral3 <leral3@naver.com>
2022-03-28 00:15:03 +0900

    객체에 모델명을 받는 set_name 메서드 추가

--- Porsche.py
+++ Porsche.py
@@ -1,10 +1,16 @@
+from statistics import mode
+
+
 class Porsche:
     def __init__(self, model, price):
         self.model = model
         self.price = price
 
+    def set_name(self, model):
+        self.model = model
 
-포르쉐 = Porsche("카이엔", "2억")
+포르쉐 = Porsche(None, None)
+
+포르쉐.set_name("카이엔")
 
 print(포르쉐.model)
-print(포르쉐.price)
